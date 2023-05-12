cffd3b555422072ffd0648a6eefebee6a7390cc3
shameemm <mandayappuramshameem@gmail.com>
2023-05-12 15:41:58 +0530

    update on backend url

--- src/Constant/Constant.js
+++ src/Constant/Constant.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://16.16.79.191/'
+export const baseUrl = 'https://cartify.website/'
 // export const API_KEY = '2ec3f9d530b953cf3167c1c5a13fefe9'
 // export const imageUrl = 'https://image.tmdb.org/t/p/original'
